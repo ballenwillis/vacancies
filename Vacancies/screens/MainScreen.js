0fd0d7a9764ef7da834fc5d8fb39e32969948e79
Ishaan Sehgal <ishaanforthewin@gmail.com>
2019-10-31 02:20:01 -0500

    Small refactor

--- Vacancies/screens/MainScreen.js
+++ Vacancies/screens/MainScreen.js
@@ -1,41 +1,8 @@
 import React from "react"
-import { StatusBar, StyleSheet, View } from "react-native"
+import { StatusBar, StyleSheet } from "react-native"
+import { withTheme, ScreenContainer, Button } from "@draftbit/ui"
 import { draftbit as screenTheme } from "../config/Themes"
-import { withTheme, ScreenContainer, Container, CardBlock, Button } from "@draftbit/ui"
-import * as PropTypes from "prop-types"
-
-const ProjectCard = () =>
-  withTheme(
-    <Container
-      style={styles.Container_nec}
-      elevation={0}
-      borderColor={props.theme.colors.divider}
-      useThemeGutterPadding={true}>
-      <CardBlock
-        style={styles.CardBlock_nve}
-        icon="MaterialIcons/cloud"
-        image="https://apps-draftbit-com.s3.amazonaws.com/apps/zYTsJNxi/assets/9e8d5125-43b4-4e87-ab69-a16a93d67a50"
-        title="[Title]"
-        elevation={1}
-        numColumns={3}
-        aspectRatio={1.5}
-        leftDescription="[user name]"
-      />
-      <Container style={styles.Container_n86} elevation={0} useThemeGutterPadding={true}>
-        <Button style={styles.Button_nl2} icon="FontAwesome/pencil" type="outline">
-          Edit Project
-        </Button>
-        <Button style={styles.Button_nl2} icon="FontAwesome/trash-o" type="outline">
-          Get Started
-        </Button>
-        <Button style={styles.Button_nl2} icon="FontAwesome/plus" type="outline">
-          Request To Join
-        </Button>
-      </Container>
-    </Container>
-  )
-
-ProjectCard.propTypes = { theme: PropTypes.any }
+import { ProjectCard } from "../components"
 
 class MainScreen extends React.Component {
   constructor(props) {
@@ -68,24 +35,6 @@ class MainScreen extends React.Component {
 }
 
 const styles = StyleSheet.create({
-  Button_n3l: {
-    top: 0,
-    left: 0,
-    right: 0,
-    position: "absolute",
-    marginHorizontal: 16,
-    marginTop: 32
-  },
-  CardBlock_nve: {
-    paddingHorizontal: 16,
-    paddingTop: 0,
-    marginTop: 32
-  },
-  Container_n86: {
-    justifyContent: "space-around",
-    flexDirection: "row",
-    marginTop: 16
-  },
   Root_nug: {
     flex: 1,
     alignItems: "center",
