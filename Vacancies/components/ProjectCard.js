650ab60c0cb554cbec00dcb35b2346e72292569e
Brandon Willis <bwmusiclover1212@gmail.com>
2019-10-31 02:29:11 -0500

    More cleanin

--- Vacancies/components/ProjectCard.js
+++ Vacancies/components/ProjectCard.js
@@ -3,9 +3,9 @@ import { StyleSheet } from "react-native"
 import * as PropTypes from "prop-types"
 import { Container, CardBlock, Button, withTheme} from "@draftbit/ui"
 
-const ProjectCard = ({theme}) => (
+const ProjectCard = ({theme, isOwner=false, isMember=false, ownerName="Brandon Willis", title="Build a dating app!", style}) => (
   <Container
-    style={styles.Container_nec}
+    style={[styles.container, style]}
     elevation={0}
     borderColor={theme.colors.divider}
     useThemeGutterPadding={true}>
@@ -13,28 +13,29 @@ const ProjectCard = ({theme}) => (
       style={styles.CardBlock_nve}
       icon="MaterialIcons/cloud"
       image="https://apps-draftbit-com.s3.amazonaws.com/apps/zYTsJNxi/assets/9e8d5125-43b4-4e87-ab69-a16a93d67a50"
-      title="[Title]"
+      title={title}
       elevation={1}
       numColumns={3}
       aspectRatio={1.5}
-      leftDescription="[user name]"
+      leftDescription={ownerName}
     />
     <Container style={styles.Container_n86} elevation={0} useThemeGutterPadding={true}>
-      <Button style={styles.Button_nl2} icon="FontAwesome/pencil" type="outline">
+      {isOwner ?<> <Button style={styles.Button_nl2} icon="FontAwesome/pencil" type="outline">
         Edit Project
       </Button>
-      <Button style={styles.Button_nl2} icon="FontAwesome/trash-o" type="outline">
-        Get Started
-      </Button>
-      <Button style={styles.Button_nl2} icon="FontAwesome/plus" type="outline">
+       <Button style={styles.Button_nl2} icon="FontAwesome/trash-o" type="outline">
+        Delete Project
+      </Button></> :
+        isMember ? null : <Button style={styles.Button_nl2} icon="FontAwesome/plus" type="outline">
         Request To Join
       </Button>
+        }
     </Container>
   </Container>);
 
 const styles = StyleSheet.create({
   Button_n3l: {
-    top: 0,
+    bottom: 0,
     left: 0,
     right: 0,
     position: "absolute",
@@ -42,15 +43,19 @@ const styles = StyleSheet.create({
     marginTop: 32
   },
   CardBlock_nve: {
-    paddingHorizontal: 16,
+    padding: 4,
     paddingTop: 0,
     marginTop: 32
   },
   Container_n86: {
     justifyContent: "space-around",
     flexDirection: "row",
-    marginTop: 16
+    marginVertical: 16
   },
+  container: {
+    borderWidth: 2,
+    borderRadius: 8
+  }
 })
 
 
