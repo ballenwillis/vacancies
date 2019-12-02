09190292286e8cbb58744528e25e0cbedf4c27ec
Ishaan Sehgal <ishaanforthewin@gmail.com>
2019-12-02 01:54:40 -0600

    Completed tab view. Need to get stacked view to work correctly. Need to get icons to display, must move on from trivial bugs, and focus big picture.

--- Vacancies/AppNavigator.js
+++ Vacancies/AppNavigator.js
@@ -1,10 +1,13 @@
 import React from "react"
 import { createAppContainer, createStackNavigator, createBottomTabNavigator } from "react-navigation"
+import Icon from 'react-native-vector-icons/FontAwesome5'
+import { Ionicons } from '@expo/vector-icons';
 import LoginScreen from "./screens/LoginScreen"
 import SignUpScreen from "./screens/SignUpScreen"
 import ProjectScreen from "./screens/ProjectScreen"
 import PostScreen from "./screens/PostScreen"
 import HomeScreen from "./screens/HomeScreen"
+
 
 const AppNavigator = createStackNavigator(
     {
@@ -30,18 +33,27 @@ const AppNavigator = createStackNavigator(
             {
                 HomeScreen: {
                     screen: HomeScreen,
-                    navigationOptions: ({ navigation }) => ({ title: "Home" }),
+                    navigationOptions: ({ navigation }) => ({ tabBarLabel: "Home",
+                        tabBarIcon: ({tintColor}) => (<Ionicons name="ios-home" size={24}  />)
+                    })
 
                 },
                 PostScreen:{
                     screen: PostScreen,
-                    navigationOptions: ({ navigation }) => ({ title: "Post" })
+                    navigationOptions: ({ navigation }) => ({ tabBarLabel: "Post",
+                        tabBarIcon: ({tintColor}) => (<Ionicons name = "ios-pizza" size = {24}/>)
+                    })
                 },
 
 
 
 
-        }),
+        }, {
+                tabBarOptions:{
+                    activeTintColor: 'red',
+                    inactiveTintColor: 'grey'
+                }
+            }),
 
         // @TODO Need to figure out way to get from project details screen back to tabbed view
         Project_details: createStackNavigator(
