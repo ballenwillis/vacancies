e2797f90ba8192be27b24fd340d656718b9d4178
Ishaan Sehgal <ishaanforthewin@gmail.com>
2019-12-02 00:33:53 -0600

    Created basic tabbed view

--- Vacancies/AppNavigator.js
+++ Vacancies/AppNavigator.js
@@ -1,52 +1,72 @@
 import React from "react"
-import { createAppContainer, createStackNavigator } from "react-navigation"
+import { createAppContainer, createStackNavigator, createBottomTabNavigator } from "react-navigation"
 import LoginScreen from "./screens/LoginScreen"
 import SignUpScreen from "./screens/SignUpScreen"
-import MainScreen from "./screens/MainScreen"
 import ProjectScreen from "./screens/ProjectScreen"
+import PostScreen from "./screens/PostScreen"
+import HomeScreen from "./screens/HomeScreen"
 
 const AppNavigator = createStackNavigator(
-  {
-    AuthNavigator: createStackNavigator(
-      {
-        LoginScreen: {
-          screen: LoginScreen
-        },
-        SignUpScreen: {
-          screen: SignUpScreen
-        }
-      },
-      {
-        headerMode: "none",
-        navigationOptions: {
-          headerVisible: false,
-          header: null
-        },
-        initialRouteName: "LoginScreen"
-      }
-    ),
-    Main_App: createStackNavigator(
-      {
-        MainScreen: {
-          screen: MainScreen,
-          navigationOptions: ({ navigation }) => ({ title: "Projects" })
-        },
-        ProjectScreen: {
-            screen: ProjectScreen,
-            navigationOptions: ({ navigation }) => ({ title: "SingleProject" })
-        }
-      },
-      {
-        navigationOptions: {
-          header: null
-        }
-      }
-    )
+    {
+        AuthNavigator: createStackNavigator(
+            {
+                LoginScreen: {
+                    screen: LoginScreen
+                },
+                SignUpScreen: {
+                    screen: SignUpScreen
+                }
+            },
+            {
+                headerMode: "none",
+                navigationOptions: {
+                    headerVisible: false,
+                    header: null
+                },
+                initialRouteName: "LoginScreen"
+            }
+        ),
+        Main_App: createBottomTabNavigator(
+            {
+                HomeScreen: {
+                    screen: HomeScreen,
+                    navigationOptions: ({ navigation }) => ({ title: "Home" }),
 
-  },
-  {
-    initialRouteName: "AuthNavigator"
-  }
+                },
+                PostScreen:{
+                    screen: PostScreen,
+                    navigationOptions: ({ navigation }) => ({ title: "Post" })
+                },
+
+
+
+
+        }),
+
+        // @TODO Need to figure out way to get from project details screen back to tabbed view
+        Project_details: createStackNavigator(
+            {
+
+
+                ProjectScreen: {
+                    screen: ProjectScreen,
+                    navigationOptions: ({ navigation }) => ({ title: "View Project" })
+                },
+
+            },
+            {
+                navigationOptions: {
+                    header: null
+                }
+            }
+        )
+
+
+
+    },
+    {
+        initialRouteName: "AuthNavigator"
+    }
 )
 const AppContainer = createAppContainer(AppNavigator)
 
