1598f8105e1060408d9e2da28d6e5138339b6f4b
Brandon Willis <bwmusiclover1212@gmail.com>
2019-12-04 14:56:50 -0600

    Getting closer

--- NoSQL/JobHandler/src/tasks/index.js
+++ NoSQL/JobHandler/src/tasks/index.js
@@ -1,1 +1,7 @@
-// export { default as SOME_JOB } from "./SOME_JOB";
+/*
+let some_job = require("./SomeJob"); // It needs to be 'some_job' the *exact* same way it is set in the job queue
+
+module.exports = {
+  some_job
+};
+*/
