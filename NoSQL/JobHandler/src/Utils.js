edbeebb78d4cf56206a5fb1ac6b725dc583047de
Brandon Willis <bwmusiclover1212@gmail.com>
2019-12-04 14:56:50 -0600

    Getting closer

--- NoSQL/JobHandler/src/Utils.js
+++ NoSQL/JobHandler/src/Utils.js
@@ -1,3 +1,54 @@
-export const loginWorker = () => {
-  // Not optional -- this function needs to be defined
-}
+const axios = require("axios");
+
+require("dotenv").config();
+
+let token = null;
+
+const sendAPIRequest = (query, variables = null) => {
+  const headers = token
+    ? {
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: `Bearer ${token}`
+        }
+      }
+    : { headers: { "Content-Type": "application/json" } };
+
+  return axios.post(
+    process.env.API_ENDPOINT,
+    {
+      query,
+      variables
+    },
+    headers
+  );
+};
+
+const loginWorker = async () => {
+  const mutation = `
+    mutation Authenticate($input: AuthenticateInput!) {
+      authenticate(input: $input) {
+        jwtToken
+      }
+    }
+  `;
+
+  const variables = {
+    input: {
+      email: process.env.EMAIL,
+      password: process.env.PASSWORD
+    }
+  };
+
+  try {
+    const response = await sendAPIRequest(mutation, variables);
+    token = response.data.data.authenticate.jwtToken;
+  } catch (e) {
+    console.log(e);
+  }
+};
+
+module.exports = {
+  loginWorker,
+  sendAPIRequest,
+};
