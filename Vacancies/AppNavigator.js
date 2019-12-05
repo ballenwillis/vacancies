58aa5d3552519b8d074ea62b9542cb0ad0255dd7
Ishaan Sehgal <ishaanforthewin@gmail.com>
2019-12-05 01:50:09 -0600

    Completed AF1 advanced query recommends jobs for applicants to apply to

--- Vacancies/AppNavigator.js
+++ Vacancies/AppNavigator.js
@@ -1,81 +1,86 @@
 import React from "react"
+import { Image } from 'react-native'
 import { createAppContainer, createStackNavigator, createBottomTabNavigator } from "react-navigation"
-import { Ionicons } from '@expo/vector-icons'
 import LoginScreen from "./screens/LoginScreen"
 import SignUpScreen from "./screens/SignUpScreen"
-import ProjectScreen from "./screens/ProjectScreen"
 import PostScreen from "./screens/PostScreen"
 import HomeScreen from "./screens/HomeScreen"
+// import ProjectScreen from "./screens/ProjectScreen"
 
 
 const AppNavigator = createStackNavigator(
-    {
-        AuthNavigator: createStackNavigator(
-            {
-                LoginScreen: {
-                    screen: LoginScreen
-                },
-                SignUpScreen: {
-                    screen: SignUpScreen
-                }
-            },
-            {
-                headerMode: "none",
-                navigationOptions: {
-                    headerVisible: false,
-                    header: null
-                },
-                initialRouteName: "LoginScreen"
-            }
-        ),
-        Main_App: createBottomTabNavigator(
-            {
-                HomeScreen: {
-                    screen: HomeScreen,
-                    navigationOptions: ({ navigation }) => ({ tabBarLabel: "Home",
-                        tabBarIcon: ({tintColor}) => ( <Ionicons name="md-checkmark-circle" size={32} color="green" />)
-                    })
+  {
+    AuthNavigator: createStackNavigator(
+      {
+        LoginScreen: {
+          screen: LoginScreen
+        },
+        SignUpScreen: {
+          screen: SignUpScreen
+        }
+      },
+      {
+        headerMode: "none",
+        navigationOptions: {
+          headerVisible: false,
+          header: null
+        },
+        initialRouteName: "LoginScreen"
+      }
+    ),
+    Main_App: createBottomTabNavigator(
+      {
+        HomeScreen: {
+          screen: HomeScreen,
+          navigationOptions: ({ navigation }) => ({ tabBarLabel: "Home",
+            tabBarIcon: ({tintColor}) => (
+              <Image
+                source={require('./assets/images/home.png')}
+                style={{width: 25, height: 25}}
+            />)
+          })
 
-                },
-                PostScreen:{
-                    screen: PostScreen,
-                    navigationOptions: ({ navigation }) => ({ tabBarLabel: "Post",
-                        tabBarIcon: ({tintColor}) => (<Ionicons name = "ios-pizza" size = {24}/>)
-                    })
-                },
-
-
-        }, {
-                tabBarOptions:{
-                    activeTintColor: 'red',
-                    inactiveTintColor: 'grey'
-                }
-            }),
-
-        // @TODO Need to figure out way to get from project details screen back to tabbed view
-        Project_details: createStackNavigator(
-            {
+        },
+        PostScreen:{
+          screen: PostScreen,
+          navigationOptions: ({ navigation }) => ({ tabBarLabel: "Post",
+            tabBarIcon: ({tintColor}) => (<Image
+              source={require('./assets/images/settings.png')}
+              style={{width: 25, height: 25}}
+            />)
+          })
+        },
 
 
-                ProjectScreen: {
-                    screen: ProjectScreen,
-                    navigationOptions: ({ navigation }) => ({ title: "View Project" })
-                },
+      }, {
+        tabBarOptions:{
+          activeTintColor: 'black',
+          inactiveTintColor: 'grey'
+        }
+      }),
 
-            },
-            {
-                navigationOptions: {
-                    header: null
-                }
-            }
-        )
+    // @TODO Need to figure out way to get from project details screen back to tabbed view
+    // Project_details: createStackNavigator(
+    //   {
+    //     ProjectScreen: {
+    //       screen: ProjectScreen,
+    //       navigationOptions: ({ navigation }) => ({ title: "View Project" })
+    //     },
+    //
+    //   },
+    //   {
+    //     navigationOptions: {
+    //       header: null
+    //     }
+    //   }
+    // )
 
 
 
-    },
-    {
-        initialRouteName: "AuthNavigator"
-    }
+  },
+  {
+    initialRouteName: "AuthNavigator"
+  }
 )
 const AppContainer = createAppContainer(AppNavigator)
 
